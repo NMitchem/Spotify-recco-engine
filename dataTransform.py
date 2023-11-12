432fe46e712d51a9a2eb17c47e28334754102c6b
Noah Mitchem <noahmitchy@gmail.com>
2023-11-12 16:12:58 -0500

    File generation and data transformation files.

---
+++ dataTransform.py
@@ -0,0 +1,187 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import re 
+import pandas as pd
+from textblob import TextBlob
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import nan_euclidean_distances
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
+
+def getSubjectivity(text):
+    '''
+    Getting the Subjectivity using TextBlob
+    '''
+    return TextBlob(text).sentiment.subjectivity
+
+def getPolarity(text):
+    '''
+    Getting the Polarity using TextBlob
+    '''
+    return TextBlob(text).sentiment.polarity
+
+def getAnalysis(score, task="polarity"):
+    '''
+    Categorizing the Polarity & Subjectivity score
+    '''
+    if task == "subjectivity":
+        if score < 1/3:
+            return "low"
+        elif score > 1/3:
+            return "high"
+        else:
+            return "medium"
+    else:
+        if score < 0:
+            return 'Negative'
+        elif score == 0:
+            return 'Neutral'
+        else:
+            return 'Positive'
+
+
+def ohe_prep(df, column, new_name): 
+    ''' 
+    Create One Hot Encoded features of a specific column
+    ---
+    Input: 
+    df (pandas dataframe): Spotify Dataframe
+    column (str): Column to be processed
+    new_name (str): new column name to be used
+        
+    Output: 
+    tf_df: One-hot encoded features 
+    '''
+    
+    tf_df = pd.get_dummies(df[column])
+    feature_names = tf_df.columns
+    tf_df.columns = [new_name + "|" + str(i) for i in feature_names]
+    tf_df.reset_index(drop = True, inplace = True)    
+    return tf_df
+
+
+
+
+def create_feature_set(df, float_cols):
+    '''
+    Process spotify df to create a final set of features that will be used to generate recommendations
+    ---
+    Input: 
+    df (pandas dataframe): Spotify Dataframe
+    float_cols (list(str)): List of float columns that will be scaled
+            
+    Output: 
+    final (pandas dataframe): Final set of features 
+    '''
+    
+    # Tfidf genre lists
+    tfidf = TfidfVectorizer()
+    tfidf_matrix = tfidf.fit_transform(df['genres'].apply(lambda x: " ".join(x)))
+    genre_df = pd.DataFrame((tfidf_matrix.toarray() + 1) * 0.18)
+    genre_df.columns = ['genre' + "|" + i for i in tfidf.get_feature_names_out()]
+    genre_df.drop(columns='genre|unknown') # drop unknown genre
+    genre_df.reset_index(drop = True, inplace=True)
+    
+    # Sentiment analysis
+    df = sentiment_analysis(df, "name")
+
+    # One-hot Encoding
+    subject_ohe = ohe_prep(df, 'subjectivity','subject') * 0.1
+    polar_ohe = ohe_prep(df, 'polarity','polar') * 0.1
+    key_ohe = ohe_prep(df, 'key','key') * 0.1
+    mode_ohe = ohe_prep(df, 'mode','mode') * 0.3
+
+    # Normalization
+    # Scale popularity columns
+    pop = df[["artist_pop","track_pop"]].reset_index(drop = True)
+    scaler = MinMaxScaler()
+    pop_scaled = pd.DataFrame(scaler.fit_transform(pop), columns = pop.columns) * 0.4 
+
+    # Scale audio columns
+    floats = df[float_cols].reset_index(drop = True)
+    scaler = MinMaxScaler()
+    floats_scaled = pd.DataFrame(scaler.fit_transform(floats), columns = floats.columns) * 0.6
+
+    # Concanenate all features
+    final = pd.concat([genre_df, floats_scaled, pop_scaled, subject_ohe, polar_ohe, key_ohe, mode_ohe], axis = 1)
+    
+    # Add song id
+    final['id']=df['id'].values
+    
+    return final
+
+def generate_artist_feature(complete_feature_set, artist_df):
+    '''
+    Summarize a user's artist into a single vector
+    ---
+    Input: 
+    complete_feature_set (pandas dataframe): Dataframe which includes all of the features for the spotify songs
+    playlist_df (pandas dataframe): playlist dataframe
+        
+    Output: 
+    complete_feature_set_playlist_final (pandas series): single vector feature that summarizes the playlist
+    complete_feature_set_nonplaylist (pandas dataframe): 
+    '''
+    
+    # Find song features in the playlist
+    complete_feature_set_playlist = complete_feature_set[complete_feature_set['id'].isin(artist_df['id'].values)]
+    # Find all non-playlist song features
+    complete_feature_set_nonplaylist = complete_feature_set[~complete_feature_set['id'].isin(artist_df['id'].values)]
+    complete_feature_set_playlist_final = complete_feature_set_playlist.drop(columns = "id")
+    return complete_feature_set_playlist_final.sum(axis = 0), complete_feature_set_nonplaylist
+
+
+
+
+def sentiment_analysis(df, text_col):
+    '''
+    Perform sentiment analysis on text
+    ---
+    Input:
+    df (pandas dataframe): Dataframe of interest
+    text_col (str): column of interest
+    '''
+    df['subjectivity'] = df[text_col].apply(getSubjectivity).apply(lambda x: getAnalysis(x,"subjectivity"))
+    df['polarity'] = df[text_col].apply(getPolarity).apply(getAnalysis)
+    return df
+    
+def generate_playlist_recos(df, features, nonplaylist_features):
+    '''
+    Generated recommendation based on songs in aspecific playlist.
+    ---
+    Input: 
+    df (pandas dataframe): spotify dataframe
+    features (pandas series): summarized playlist feature (single vector)
+    nonplaylist_features (pandas dataframe): feature set of songs that are not in the selected playlist
+        
+    Output: 
+    non_playlist_df_top_40: Top 40 recommendations for that playlist
+    '''
+    
+    non_playlist_df = df[df['id'].isin(nonplaylist_features['id'].values)]
+    # Find cosine similarity between the playlist and the complete song set
+    X = nonplaylist_features.drop('id', axis = 1).values
+    y = features.values.reshape(1, -1)
+    non_playlist_df['sim'] = cosine_similarity(X, y)[:,0]
+    non_playlist_df_top_40 = non_playlist_df.sort_values('sim',ascending = False).head(40)
+    
+    return non_playlist_df_top_40
+
+
+if __name__ == "__main__":
+    data = pd.read_csv("songs.csv")
+    unfiltered = pd.read_csv("songs.csv")
+    #Drop useless info
+    data = data.drop(['type', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature'], axis = 1)
+
+    #Split each row's genres into a list
+    data["genres"] = data.genres.str.split(" ")
+    data['name'] = data.name.astype(str)
+    float_cols = data.dtypes[data.dtypes == 'float64'].index.values
+    temp = create_feature_set(data, float_cols)
+
+    artist = "The Strokes"
+    complete_feature_set_playlist_vector, complete_feature_set_nonplaylist = generate_artist_feature(temp, unfiltered[unfiltered["artist"] == artist])
+    recommend = generate_playlist_recos(data, complete_feature_set_playlist_vector, complete_feature_set_nonplaylist)
+    recommend.to_csv("recommend.csv", index = False)
---
+++ fileGeneration.py
@@ -0,0 +1,109 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import re 
+import pandas as pd
+from textblob import TextBlob
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Load clientID and Secret from your .env file 
+load_dotenv()
+cid = os.environ.get("CID")
+secret = os.environ.get("SECRET")
+
+def playlist_grab(users):
+    songs = []
+    for user in users:
+        playlists = sp.user_playlists(user)
+        while playlists:
+            for playlist in playlists['items']:
+                songs.append([playlist["name"], playlist['uri']])
+            if playlists['next']:
+                playlists = sp.next(playlists)
+            else:
+                playlists = None
+    return songs
+
+def playlist_extract(playlist_name, uri):
+   # Define a common list to extract data to
+    all_data = []
+
+   # Define an inner function to grab data and manipulate it 
+    def tracks_to_features(uris, playlist_name):
+        feature_list = sp.audio_features(uris)
+        tracks = sp.tracks(uris)
+        tracks = tracks["tracks"]
+        artists = sp.artists([tracks[i]["artists"][0]["id"] for i in range(len(tracks))])["artists"]
+        for i, features in enumerate(feature_list):
+            #Artist of the track, for genres and popularity
+            track = tracks[i]
+            artist = track["artists"][0]["name"]
+            artist_data = artists[i]
+            artist_pop = artist_data["popularity"]
+            artist_genres = artist_data["genres"]
+
+            #Track popularity
+            track_pop = track["popularity"]
+
+            #Add in extra features
+            try:
+                features["artist_pop"] = artist_pop
+            except:
+                continue
+            if artist_genres:
+                features["genres"] = " ".join([re.sub(' ','_',i) for i in artist_genres])
+            else:
+                features["genres"] = "unknown"
+            features["track_pop"] = track_pop
+            features["playlist_name"] = playlist_name
+            features["artist"] = artist
+            features["name"] = track["name"]
+            all_data.append(features)
+
+    playlist = sp.playlist(uri)
+    tracks = []
+    for i in playlist["tracks"]["items"]:
+        try:
+            tracks.append(i["track"]["uri"])
+        except:
+            1==1
+    length = len(tracks)
+    #Can only process 50 tracks at a time via API
+    if length > 50:
+        for i in range(0, int(length / 50)):
+            subset = tracks[i * 50: (i + 1) * 50]
+            try:
+                tracks_to_features(subset, playlist_name)
+            except:
+                # Bad track in the data
+                1==1
+    else:
+        try:
+            tracks_to_features(tracks, playlist_name)
+        except:
+            return all_data
+    return all_data
+
+def drop_duplicates(df):
+    df['artists_song'] = df.apply(lambda row: row['name'] + row['artist'], axis = 1)
+    return df.drop_duplicates('artists_song').reset_index(drop = True).drop("artists_song", axis = 1)
+    
+if __name__ == "__main__":
+    #Authentication - without user
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+    #Define users to grab playlists from
+    users = ["spotify", "digster.fm", "myplay.com", "topsify", "sanik007"]
+    songs = playlist_grab(users)
+    df_list = []
+    for i in songs:
+        df_list = df_list + playlist_extract(i[0], i[1])
+    data = pd.DataFrame(df_list)
+    data.to_csv('songs.csv', index = False)
+    data = drop_duplicates(data)
+    data.to_csv('songs.csv', index = False)
